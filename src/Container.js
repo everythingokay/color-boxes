eceb86e75b5c628a91b28946fd0f4356d7928514
Alison Feldish <alison.feldish@gmail.com>
2022-12-02 14:32:46 -0500

    finish styling

--- src/Container.js
+++ src/Container.js
@@ -5,7 +5,20 @@ import "./Container.css";
 class Container extends Component {
     static defaultProps = {
         numBoxes: 18,
-        colors: ["#304D63", "#B2E7E8", "#8FB9AA", "#F2D096", "#ED8975"]
+        colors: [
+            "#304D63", 
+            "#B2E7E8", 
+            "#8FB9AA", 
+            "#F2D096", 
+            "#ED8975",
+            "#A16B56",
+            "#E0A370",
+            "#DEC584",
+            "#E2D7A7",
+            "#709F9D",
+            "#567D89"
+            
+        ]
     };
     render() {
         const boxes = Array.from({ length: this.props.numBoxes }).map(() => 
